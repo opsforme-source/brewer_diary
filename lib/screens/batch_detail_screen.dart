cb97aefdc8ae509038995f678ae4c5c8a280e944
opsforme-source <opsforme@gmail.com>
2026-06-15 15:50:11 +0200

    Import brew status extension in batch detail

--- lib/screens/batch_detail_screen.dart
+++ lib/screens/batch_detail_screen.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 
 import '../controllers/batch_controller.dart';
 import '../models/brew_batch.dart';
+import '../models/brew_status.dart';
 import '../models/gravity_reading.dart';
 import '../models/ingredient.dart';
 import 'batch_edit_screen.dart';
